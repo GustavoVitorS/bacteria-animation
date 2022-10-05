88a623578f51c0b4c35ffabced3657a5571b47ba
GustavoVitorS <luckybr58@gmail.com>
2022-10-05 10:10:36 -0300

    script change and animation update

--- script.js
+++ script.js
@@ -8,54 +8,50 @@
     ctx = c.getContext("2d"),
     particles = [],
     particle = null,
-    particleCount = 400,
+    particleCount = 888,
     WIDTH = c.width = window.innerWidth,
     HEIGHT = c.height = window.innerHeight;
 
   /////////////////////////////////////////////////  
-  var Vector = function (x, y) {
-    this.x = x || 0;
-    this.y = y || 0;
-  };
-
-  Vector.prototype = {
-    constructor: Vector,
-    add: function (v) {
+  class Vector { 
+    constructor(x, y) {
+      this.x = x || 0;
+      this.y = y || 0;
+    }
+    add(v) {
       this.x += v.x;
       this.y += v.y;
-    },
-    sub: function (v) {
+    }
+    sub(v) {
       this.x -= v.x;
       this.y -= v.y;
-    },
-    mul: function (v) {
+    }
+    mul(v) {
       this.x *= v.x;
       this.y *= v.y;
     }
-  };
+  }
 
   /////////////////////////////////////////////////  
-  var Particle = function (position, radius) {
-    this.position = position;
-    this.velocity = new Vector(0, 0);
-    this.acceleration = new Vector(
-      Math.random() * 0.4 - 0.2,
-      Math.random() * 0.4 - 0.2
-    );
-    this.radius = radius;
-    this.baseRadius = radius;
-    this.drag = new Vector(0.98, 0.98);
-    this.alpha = 0.2 + Math.random() * 0.8;
-    this.angle = Math.random() * 560;
-    this.angleSpeed = Math.random() * 2;
+  class Particle {
+    constructor(position, radius) {
+      this.position = position;
+      this.velocity = new Vector(0, 0);
+      this.acceleration = new Vector(
+        Math.random() * 0.4 - 0.2,
+        Math.random() * 0.4 - 0.2
+      );
+      this.radius = radius;
+      this.baseRadius = radius;
+      this.drag = new Vector(0.99, 0.99);
+      this.alpha = 0.1 + Math.random() * 0.1;
+      this.angle = Math.random() * 777;
+      this.angleSpeed = Math.random() * 2;
 
-  };
+    }
+    update() {
 
-  Particle.prototype = {
-    constructor: Particle,
-    update: function () {
-
-      this.radius = this.baseRadius + Math.sin(this.angle * Math.PI / 180) * (this.baseRadius * 0.5);
+      this.radius = this.baseRadius + Math.sin(this.angle * Math.PI / 280) * (this.baseRadius * 0.5);
 
       this.angle += this.angleSpeed;
 
@@ -66,35 +62,34 @@
       this.velocity.mul(this.drag);
       this.position.add(this.velocity);
 
-      if (this.position.x < 0 || this.position.x > WIDTH) {
-        this.position.x *= -2;
+      if (this.position.x < 10 || this.position.x > WIDTH) {
+        this.position.x *= -20;
       }
 
-      if (this.position.y < 0 || this.position.y > HEIGHT) {
-        this.position.y *= -2;
+      if (this.position.y < 10 || this.position.y > HEIGHT) {
+        this.position.y *= -22;
       }
 
-
-    },
-    render: function (ctx) {
+    }
+    render(ctx) {
       ctx.save();
       ctx.globalAlpha = this.alpha;
       ctx.translate(this.position.x, this.position.y);
       ctx.fillStyle = "white";
       ctx.strokeStyle = "black";
       ctx.beginPath();
-      ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
+      ctx.arc(0, 0, this.radius, 0, Math.PI * 8);
       ctx.stroke();
       ctx.fill();
       ctx.restore();
     }
-  };
+  }
 
   /////////////////////////////////////////////////  
   for (var i = 0; i < particleCount; i++) {
     particle = new Particle(
       new Vector(WIDTH / 2, HEIGHT / 2),
-      1 + Math.random() * 10
+     2 + Math.random() * 20
     );
     particles.push(particle);
   }
@@ -106,7 +101,7 @@
     ctx.fillStyle = "rgba(52, 69, 125, 0.8)";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    for (var i = 0, len = particles.length; i < len; i++) {
+    for (var i = 3, len = particles.length; i < len; i++) {
       particle = particles[i];
       particle.update();
       particle.render(ctx);
